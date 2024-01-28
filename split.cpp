79628c7007bfa1d23c2d610b6105ff808ae1206f
Rishi Sujit <sujit@usc.edu>
2024-01-28 00:53:29 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -11,13 +11,44 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
 /* Add a prototype for a helper function here if you need */
+
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+  // WRITE YOUR CODE HERE
+
+  //initialzing the lists to be empty
+  odds = NULL;
+  evens = NULL;
+
+  //while their is values in the input list
+  while(in != NULL)
+  {
+    //pointer that keeps track of the current node
+    Node* curr = in;
+    in = in->next;
+
+    //checking if val is even
+    if (curr->value % 2 == 0)
+    {
+      //add to correct list
+      curr->next = evens;
+      evens = curr;
+
+    }
+    //checking if odd
+    else
+    {
+      //adding to list
+      curr->next = odds;
+      odds = curr;
+    }
+  }
+  
 }
 
 /* If you needed a helper function, write it here */
